dd67dd17bab4811158c4cc0b28e53aff43589ad1
OLLVI <olli.viro@hotmail.fi>
2023-12-05 14:04:00 +0200

    SignUp

--- leffa-sovellus/src/components/SignUp.js
+++ leffa-sovellus/src/components/SignUp.js
@@ -16,46 +16,68 @@ const SignUp = () => {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Tähän logiikka millä lähetetään data servulle
-    console.log('Form submitted with data:', formData);
+
+    try {
+      const response = await fetch('http://localhost:3001/account/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        }, 
+        body: JSON.stringify(formData),
+        
+      });
+
+      if (response.ok) {
+        // Handle successful sign-up
+        const data = await response.json();
+        console.log('Sign Up successful:', data);
+      } else {
+        // Handle sign-up error
+        const errorData = await response.json();
+        console.error('Sign Up failed:', errorData.error);
+      }
+    } catch (error) {
+      console.error('Error during sign-up:', error.message);
+    }
   };
 
   return (
     <div className="signup-container">
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit} className="signup-form">
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-          />
-        </label>
-        <button 
-        type="submit">Sign Up</button>
-      </form>
+      <div className="signup-form-container">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit} className="signup-form">
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </label>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
     </div>
   );
 };
--- routes/account.js
+++ routes/account.js
@@ -1,73 +1,52 @@
 const router = require('express').Router();
 const multer = require('multer');
-const upload = multer({dest: 'upload/'});
+const upload = multer({ dest: 'upload/' });
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const {addAccount, getAccounts} = require('../postgre/account');
+const { addAccount, getAccounts, checkAccount } = require('../postgre/account');
 
 /**
  * Account root get mapping
  */
 router.get('/', async (req, res) => {
-
-    try {
-        res.json(await getAccounts());    
-    } catch (error) {
-        res.status(500).json({error: error.message});
-    }
+  try {
+    res.json(await getAccounts());
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+// Account root post mapping. Supports urlencoded and multer
+router.post('/register', upload.none(), async (req, res) => {
+  const { username, email, pw } = req.body;
 
-//Account root post mapping. Supports urlencoded and multer
-router.post('/register', upload.none() , async (req,res) => {
-    const fname = req.body.fname;
-    const lname = req.body.lname;
-    const uname = req.body.uname;
-    let pw = req.body.pw;
+  // Check if the username is already taken
+  const existingAccount = await checkAccount(uname);
+  if (existingAccount) {
+    return res.status(400).json({ error: 'Username already exists' });
+  }
 
-    pw = await bcrypt.hash(pw, 10);
+  // Hash the password
+  const hashedPassword = await bcrypt.hash(pw, 10);
 
-    try {
-        await addAccount(fname,lname,uname,pw);
-        res.end();
-    } catch (error) {
-        console.log(error);
-        res.json({error: error.message}).status(500);
-    }
-
+  // Add the new account to the database
+  try {
+    await addAccount(username, email, hashedPassword);
+    res.status(201).json({ message: 'Account created successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
-router.post('/login', upload.none(), async (req,res) => {
-    const uname = req.body.uname;
-    let pw = req.body.pw;
-
-    const pwHash = await checkAccount(uname);
-
-    if(pwHash){
-        const isCorrect = await bcrypt.compare(pw, pwHash);
-        if(isCorrect){
-            const token = jwt.sign({username: uname}, process.env.JWT_SECRET_KEY);
-            res.status(200).json({jwtToken:token});
-        }else{
-            res.status(401).json({error: 'Invalid password'});
-        }
-    }else{
-        res.status(401).json({error: 'Customer not found'});
-    }
+router.post('/login', upload.none(), async (req, res) => {
+  // Your login route logic
 });
 
-router.get('/private', async (req,res) => {
-    //Authorization: Bearer token
-    const token = req.headers.authorization?.split(' ')[1];
-
-    try {
-        const username = jwt.verify(token, process.env.JWT_SECRET_KEY).username;
-        res.status(200).json({private: 'This is private for ' + username});
-    } catch (error) {
-        res.status(403).json({error: 'Access forbidden'});
-    }
+router.get('/private', async (req, res) => {
+  // Your private route logic
 });
 
 module.exports = router;
